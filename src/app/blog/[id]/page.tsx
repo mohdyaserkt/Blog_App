82d2bd6400b82215db212187819c3721f41b016b
mohdyaserkt <yaserkt786@gmail.com>
2023-07-15 22:59:01 +0000

    16 Jul 2023, 04:29

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
+import styles from './page.module.css';
 
-const Post = () => {
+
+
+
+
+const BlogPost: React.FC = () => {
   return (
-    <div>Post</div>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <div className={styles.info}>
+          <h1 className={styles.title}>data.title</h1>
+          <p className={styles.desc}>data.desc</p>
+          <div className={styles.author}>
+            <Image
+              src="https://images.pexels.com/photos/10474144/pexels-photo-10474144.jpeg"
+              alt=""
+              width={40}
+              height={40}
+              className={styles.avatar}
+            />
+            <span className={styles.username}>yaserkt786</span>
+          </div>
+        </div>
+        <div className={styles.imageContainer}>
+          <Image
+            src="https://images.pexels.com/photos/10474144/pexels-photo-10474144.jpeg"
+            alt=""
+            layout="fill"
+            objectFit="cover"
+            className={styles.image}
+          />
+        </div>
+      </div>
+      <div className={styles.content}>
+        <p className={styles.text}>data.content</p>
+      </div>
+    </div>
+  );
+};
 
-export default Post
+export default BlogPost;
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,10 +1,33 @@
-import React from 'react'
-import styles from './page.module.css'
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import styles from './page.module.css';
 
-const Blog = () => {
+
+
+
+const Blog: React.FC = () => {
   return (
-    <div>Blog</div>
-  )
-}
+    <div className={styles.mainContainer}>
+      
+        <Link href='/blog/testid' className={styles.container} >
+          <div className={styles.imageContainer}>
+            <Image
+              src="https://images.pexels.com/photos/10474144/pexels-photo-10474144.jpeg"
+              alt=""
+              width={400}
+              height={250}
+              className={styles.image}
+            />
+          </div>
+          <div className={styles.content}>
+            <h1 className={styles.title}>Test</h1>
+            <p className={styles.desc}>desc</p>
+          </div>
+        </Link>
+      
+    </div>
+  );
+};
 
-export default Blog
+export default Blog;
