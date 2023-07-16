48573ea8c3d2b631510441678ea233a488badf79
mohdyaserkt <yaserkt786@gmail.com>
2023-07-16 05:08:48 +0000

    16 Jul 2023, 10:38

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,34 @@
-import React from 'react'
+"use client"
+import React, { useEffect, useState } from 'react'
 import styles from './page.module.css'
 
 const Dashboard = () => {
+const [data, setdata] = useState([])
+const [loading, setloading] = useState(false)
+const [error, seterror] = useState(false)
+  useEffect(() => {
+
+    async function getData() {
+      setloading(true)
+      const res = await fetch(`https://jsonplaceholder.typicode.com/posts/`)
+      
+      if (!res.ok) {
+        seterror(true)
+      }
+      const data=await res.json()
+    
+      setdata(data)
+      setloading(false) 
+    
+    }
+    getData()
+    
+  
+    
+  }, [])
+  console.log(data);
+  
+  
   return (
     <div>Dashboard</div>
   )
