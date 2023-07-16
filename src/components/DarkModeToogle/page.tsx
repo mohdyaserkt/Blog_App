c1e599863dbf90f12d53f9076f29d572d947167c
mohdyaserkt <yaserkt786@gmail.com>
2023-07-16 00:14:04 +0000

    16 Jul 2023, 05:44

--- src/components/DarkModeToogle/page.tsx
+++ src/components/DarkModeToogle/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React, { useContext } from "react";
 import styles from "./page.module.css";
 import { ThemeContext } from "../../../context/themeContext";
@@ -7,12 +8,12 @@ const DarkModeToggle: React.FC = () => {
     const { toggle,mode } = useContext(ThemeContext);
   
   return (
-    <div className={styles.container} >
+    <div className={styles.container} onClick={toggle} >
       <div className={styles.icon}>🌙</div>
       <div className={styles.icon}>🔆</div>
       <div
         className={styles.ball}
-        // style={mode === "light" ? { left: "2px" } : { right: "2px" }}
+         style={mode === "light" ? { left: "2px" } : { right: "2px" }}
         
       />
     </div>
