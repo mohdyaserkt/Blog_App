37faa2ba381da0ca223b94a88474a367e0353266
mohdyaserkt <yaserkt786@gmail.com>
2023-07-16 05:56:38 +0000

    16 Jul 2023, 11:26

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,32 +1,36 @@
 "use client"
 import React, { useEffect, useState } from 'react'
 import styles from './page.module.css'
+import useSWR from 'swr'
+ 
+
 
 const Dashboard = () => {
-const [data, setdata] = useState([])
-const [loading, setloading] = useState(false)
-const [error, seterror] = useState(false)
-  useEffect(() => {
+// const [data, setdata] = useState([])
+// const [loading, setloading] = useState(false)
+// const [error, seterror] = useState(false)
+//   useEffect(() => {
 
-    async function getData() {
-      setloading(true)
-      const res = await fetch(`https://jsonplaceholder.typicode.com/posts/`)
+//     async function getData() {
+//       setloading(true)
+//       const res = await fetch(`https://jsonplaceholder.typicode.com/posts/`)
       
-      if (!res.ok) {
-        seterror(true)
-      }
-      const data=await res.json()
+//       if (!res.ok) {
+//         seterror(true)
+//       }
+//       const data=await res.json()
     
-      setdata(data)
-      setloading(false) 
+//       setdata(data)
+//       setloading(false) 
     
-    }
-    getData()
+//     }
+//     getData()
     
   
     
-  }, [])
-  console.log(data);
+//   }, [])
+//   console.log(data);
+
   
   
   return (
