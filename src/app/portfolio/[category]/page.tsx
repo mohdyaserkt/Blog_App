46122fb0746a790f46b894ced186b6f981c9519f
mohdyaserkt <yaserkt786@gmail.com>
2023-07-16 06:25:17 +0000

    16 Jul 2023, 11:55

--- src/app/portfolio/[category]/page.tsx
+++ src/app/portfolio/[category]/page.tsx
@@ -3,66 +3,64 @@ import Image from 'next/image';
 
 import styles from './page.module.css';
 import Button from '@/components/Button/Button';
+import { items } from './data';
+import { notFound } from 'next/navigation';
 
 interface CategoryProps {
   params: { category: string };
 }
 
+type Item = {
+  id: number;
+  title: string;
+  desc: string;
+  image: string;
+};
+
+type Items = {
+  applications: Item[];
+  illustrations: Item[];
+  websites: Item[];
+};
+
+const getdata = (cat: keyof Items) => {
+  const data = items[cat];
+  if (data) {
+    return data
+  }
+  return notFound()
+};
 
 
 const Category: React.FC<CategoryProps> = ({ params }) => {
-  
+
+  const data = getdata(params.category as keyof Items)
+
 
   return (
     <div className={styles.container}>
       <h1 className={styles.catTitle}>{params.category}</h1>
+      {data.map((item) => (
+
+        <div className={styles.item}  key={item.id}>  
+          <div className={styles.content}>
+            <h1 className={styles.title}>{item.title}</h1>
+            <p className={styles.desc}>{item.desc}</p>
+            <Button text="See More" url="#" />
+          </div>
+          <div className={styles.imgContainer}>
+            <Image
+              className={styles.img}
+              fill={true}
+              src={item.image}
+              alt=""
+            />
+          </div>
+        </div>
+      ))}
 
 
-      <div className={styles.item} >
-        <div className={styles.content}>
-          <h1 className={styles.title}>Test</h1>
-          <p className={styles.desc}>Desc</p>
-          <Button text="See More" url="#" />
-        </div>
-        <div className={styles.imgContainer}>
-          <Image
-            className={styles.img}
-            fill={true}
-            src="https://images.pexels.com/photos/10474131/pexels-photo-10474131.jpeg"
-            alt=""
-          />
-        </div>
-      </div>
-      <div className={styles.item} >
-        <div className={styles.content}>
-          <h1 className={styles.title}>Test</h1>
-          <p className={styles.desc}>Desc</p>
-          <Button text="See More" url="#" />
-        </div>
-        <div className={styles.imgContainer}>
-          <Image
-            className={styles.img}
-            fill={true}
-            src="https://images.pexels.com/photos/10474131/pexels-photo-10474131.jpeg"
-            alt=""
-          />
-        </div>
-      </div>
-      <div className={styles.item} >
-        <div className={styles.content}>
-          <h1 className={styles.title}>Test</h1>
-          <p className={styles.desc}>Desc</p>
-          <Button text="See More" url="#" />
-        </div>
-        <div className={styles.imgContainer}>
-          <Image
-            className={styles.img}
-            fill={true}
-            src="https://images.pexels.com/photos/10474131/pexels-photo-10474131.jpeg"
-            alt=""
-          />
-        </div>
-      </div>
+
 
     </div>
   );
