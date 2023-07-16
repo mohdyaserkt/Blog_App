5fef0f8ba89497b205376192f1ec7a8f7ae2f585
mohdyaserkt <yaserkt786@gmail.com>
2023-07-16 13:30:24 +0000

    16 Jul 2023, 19:00

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server"
 
-export const GET = async(request)=>{
+export const GET = async(request: any)=>{
 
     return new NextResponse("its not works",{status:200});
 };
