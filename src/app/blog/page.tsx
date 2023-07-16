a786e604910ebfee3f0ab6b13d6b06f7bb169c50
mohdyaserkt <yaserkt786@gmail.com>
2023-07-16 00:33:55 +0000

    16 Jul 2023, 06:03

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -2,15 +2,35 @@ import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import styles from './page.module.css';
+import { json } from 'stream/consumers';
 
 
 
+async function getData() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
 
-const Blog: React.FC = () => {
+  // Recommendation: handle errors
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data')
+  }
+
+  return res.json()
+
+}
+
+
+
+const Blog: React.FC = async () => {
+  const data = await getData()
   return (
+
+
     <div className={styles.mainContainer}>
-      
-        <Link href='/blog/testid' className={styles.container} >
+      {data.map((item: { id: React.Key | null | undefined; title: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | React.PromiseLikeOfReactNode | null | undefined; }) => {
+        <Link href='/blog/testid' className={styles.container} key={item.id}>
           <div className={styles.imageContainer}>
             <Image
               src="https://images.pexels.com/photos/10474144/pexels-photo-10474144.jpeg"
@@ -21,11 +41,13 @@ const Blog: React.FC = () => {
             />
           </div>
           <div className={styles.content}>
-            <h1 className={styles.title}>Test</h1>
+            <h1 className={styles.title}>{item.title}</h1>
             <p className={styles.desc}>desc</p>
           </div>
         </Link>
-      
+      })}
+
+
     </div>
   );
 };
