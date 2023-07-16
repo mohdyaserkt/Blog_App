d1c96720e3e1ded134f176349e3f563a00968bae
mohdyaserkt <yaserkt786@gmail.com>
2023-07-16 12:25:20 +0000

    16 Jul 2023, 17:55

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -1,11 +1,35 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextApiRequest, NextApiResponse } from 'next';
 
-export const getPosts = async (request: NextRequest): Promise<NextResponse> => {
-  return new NextResponse("it works!!", { status: 200 });
-};
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'GET') {
+    handleGet(req, res);
+  } else if (req.method === 'POST') {
+    handlePost(req, res);
+  } else if (req.method === 'PUT') {
+    handlePut(req, res);
+  } else if (req.method === 'DELETE') {
+    handleDelete(req, res);
+  } else {
+    res.status(405).json({ message: 'Method Not Allowed' });
+  }
+}
 
-// export const postPosts = async (request: NextRequest): Promise<NextResponse> => {
-//   // Handle POST request logic here
-// };
+function handleGet(req: NextApiRequest, res: NextApiResponse) {
+  // Handle GET request logic here
+  res.status(200).json({ message: 'GET request handled successfully' });
+}
 
-// Export other HTTP methods as needed (e.g., putPosts, deletePosts, etc.)
+function handlePost(req: NextApiRequest, res: NextApiResponse) {
+  // Handle POST request logic here
+  res.status(200).json({ message: 'POST request handled successfully' });
+}
+
+function handlePut(req: NextApiRequest, res: NextApiResponse) {
+  // Handle PUT request logic here
+  res.status(200).json({ message: 'PUT request handled successfully' });
+}
+
+function handleDelete(req: NextApiRequest, res: NextApiResponse) {
+  // Handle DELETE request logic here
+  res.status(200).json({ message: 'DELETE request handled successfully' });
+}
