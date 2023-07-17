9384dd6c8fa1657f565e71b441e35021646aef91
mohdyaserkt <yaserkt786@gmail.com>
2023-07-17 06:36:28 +0000

    17 Jul 2023, 12:06

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,6 +3,8 @@ import React, { useEffect, useState } from 'react'
 import styles from './page.module.css'
 import useSWR from 'swr'
 import { useSession } from 'next-auth/react'
+import { useRouter } from 'next/navigation'
+
  
 
 
@@ -34,11 +36,24 @@ const Dashboard = () => {
 
   const session = useSession() 
   console.log(session);
-  
-  
-  return (
+  const router = useRouter()
+
+  if(session.status==="loading"){
+    return <p>Loading</p>
+  }
+
+  if(session.status==="unauthenticated"){
+    router?.push("/dashboard/login")
+  }
+
+  if(session.status==="authenticated"){
+    return (
     <div>Dashboard</div>
   )
+  }
+  
+  
+  
 }
 
 export default Dashboard
