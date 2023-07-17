e8b63647d24731cfd6f1bca08c264c9397287a74
mohdyaserkt <yaserkt786@gmail.com>
2023-07-17 04:35:02 +0000

    17 Jul 2023, 10:05

--- src/app/dashboard/(Auth)/register/page.tsx
+++ src/app/dashboard/(Auth)/register/page.tsx
@@ -2,12 +2,15 @@
 import React, { useState } from 'react'
 import styles from './page.module.css'
 import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 
 const register = () => {
 
   const [error, setError] = useState(false)
+  const router= useRouter()
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
+
+  const handleSubmit = async(e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     const form = e.target as HTMLFormElement;
@@ -19,47 +22,63 @@ const register = () => {
     const email: string = emailInput.value;
     const password: string = passwordInput.value;
 
-    // Rest of your code
+
+    try {
+      const res = await fetch("/api/auth/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name,
+          email,
+          password,
+        }),
+      });
+
+      res.status===201&&router.push("/dashboard/login?success=Accout has been created")
+
+      // Rest of your code
+    } catch (error) {
+      // Error handling
+    }
+
   };
 
-  try {
-    
-  } catch (error) {
-    
-  }
+
 
   return (
     <div className={styles.container}>
-    <h1 className={styles.title}>Create an Account</h1>
-    <h2 className={styles.subtitle}>Please sign up to see the dashboard.</h2>
-    <form  className={styles.form} onSubmit={handleSubmit}>
-      <input
-        type="text"
-        placeholder="Username"
-        required
-        className={styles.input}
-      />
-      <input
-        type="text"
-        placeholder="Email"
-        required
-        className={styles.input}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        required
-        className={styles.input}
-      />
-      <button type="submit" className={styles.button}>Register</button>
-      {error && "Something went wrong!"}
-      
-    </form>
-    <span className={styles.or}>- OR -</span>
-    <Link href="/dashboard/login" passHref legacyBehavior>
-      <a className={styles.link}>Login with an existing account</a>
-    </Link>m  
-  </div>
+      <h1 className={styles.title}>Create an Account</h1>
+      <h2 className={styles.subtitle}>Please sign up to see the dashboard.</h2>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Username"
+          required
+          className={styles.input}
+        />
+        <input
+          type="text"
+          placeholder="Email"
+          required
+          className={styles.input}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          required
+          className={styles.input}
+        />
+        <button type="submit" className={styles.button}>Register</button>
+        {error && "Something went wrong!"}
+
+      </form>
+      <span className={styles.or}>- OR -</span>
+      <Link href="/dashboard/login" passHref legacyBehavior>
+        <a className={styles.link}>Login with an existing account</a>
+      </Link>m
+    </div>
   )
 }
 
