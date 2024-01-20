95e0558f89045d45a102b36a2419342f6494d026
mohdyaserkt <yaserkt786@gmail.com>
2024-01-20 09:02:33 +0000

    20 Jan 2024, 14:32

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -8,7 +8,7 @@ import { notFound } from 'next/navigation';
 
 
 async function getData() {
-  const res = await fetch('http://localhost:3000/api/posts',r)
+  const res = await fetch('http://localhost:3000/api/posts')
   if (!res.ok) {
     return notFound()
   }
